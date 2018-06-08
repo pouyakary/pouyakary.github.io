f4105b08f9a39c9e35f267e12d05237025a27afd
pmkary <pouya@kary.us>
2018-06-08 18:43:32 +0430

    updated the script to load the table of the contents faster because we need it

--- scripts/topic-toc.js
+++ scripts/topic-toc.js
@@ -17,11 +17,11 @@
 
     document.addEventListener( "loadeddata", TableOfContents )
 
+    TableOfContents( )
+
     window.onload = function ( ) {
         initElementsOnload( )
         initWindowEventsOnLoad( )
-
-        TableOfContents( )
         updateTableOfContentsInLocation( )
         reEvaluateSidebarOverflowShadows( )
     }
